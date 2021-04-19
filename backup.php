421638ffff7db2692ac6001568ec318bc52fa0f4
Christophoros Prodromou <52533416+Chrisprod06@users.noreply.github.com>
2021-04-19 18:04:33 +0300

    fixed some spelling issues and styling to back and restore

--- backup.php
+++ backup.php
@@ -4,116 +4,118 @@ include_once 'includes/header.inc.php';
 ?>
 
 <style>
-    .btn-success {
-        margin-left: -10px;
-    }
+  .btn-success {
+    margin-left: -10px;
+  }
 
-    .file-move {
-        margin-right: 50px;
-    }
+  .file-move {
+    margin-right: 50px;
+  }
 </style>
 
 <!-- Begin Page Content -->
 <div class="container-fluid">
 
-    <!-- Page Heading -->
+  <!-- Page Heading -->
 
-    <!-- Content Row -->
+  <!-- Content Row -->
 
 
-    <div class="table-wrapper">
-        <div class="table-title">
-            <div class="row">
-            <div class="card">
-                <div class="card-header d-flex align-items-center">
-                  <h4><?php echo $lang['autobackup']?></h4>
-                </div>
-            <div class="card-body">
-                <form action="includes/autobackup.inc.php" method="POST" >
-                  <div class="col-sm-6">
-                  <?php
-                  if(isset($_GET['b'])){
-                  if($_GET['b']=='success'){
-                  echo '<p><b> Successfully Updated. </b></p>';}
-                  else if($_GET['b']=='fail'){
-                    echo '<p><b> A problem has occured. </b></p>';}}
-                  ?>
-                    <div class="line"></div>
-                    <p><b><?php echo $lang['note']?></b><?php echo $lang['autobackupnote']?></p>
-                    <p><b><?php echo $lang['interval']?></b> <?php echo $lang['every']?> <b><?php
-                    
-                    include_once 'includes/dbh.inc.php';
+  <div class="table-wrapper">
+    <div class="table-title">
+      <div class="row">
+        <div class="card">
+          <div class="card-header d-flex align-items-center">
+            <h4><?php echo $lang['autobackup'] ?></h4>
+          </div>
+          <div class="card-body">
+            <form action="includes/autobackup.inc.php" method="POST">
+              <div class="col-sm-6">
+                <?php
+                if (isset($_GET['b'])) {
+                  if ($_GET['b'] == 'success') {
+                    echo '<p><b> Successfully Updated. </b></p>';
+                  } else if ($_GET['b'] == 'fail') {
+                    echo '<p><b> A problem has occured. </b></p>';
+                  }
+                }
+                ?>
+                <div class="line"></div>
+                <p><b><?php echo $lang['note'] ?></b><?php echo $lang['autobackupnote'] ?></p>
+                <p><b><?php echo $lang['interval'] ?></b> <?php echo $lang['every'] ?> <b><?php
 
-                    $getQuery2 = "SELECT * from autobackup WHERE bID=(SELECT MAX(bID) FROM autobackup);";
-                    $setQuery2 = mysqli_query($conn, $getQuery2);
-                    $searchProp = array();
-                    
-                    while ($row = mysqli_fetch_assoc($setQuery2)) 
-                    {
-                    
-                        $searchProp[] = array(
-                      
-                          $lastbackup = $row['lastbackup'],
-                          $autointerval = $row['autointerval']
-                        );  
-                       
-                    }
-                    
-                    echo $autointerval;
-                    
-                    ?></b> days. </p>
-                    <div class="line"></div>
-                    <div class="col-sm-6 mb-3">
-                      <select name="autointerval" class="form-control" required data-error="Please select the interval.">
-                      <option value=''></option>
-                        <option value='7'><?php echo $lang['every7']?></option>
-                        <option value='30'><?php echo $lang['every30']?></option>
-                        <option value='60'><?php echo $lang['every60']?></option>
-                        <option value='90'><?php echo $lang['every90']?></option>
-                      </select>
-                    </div>
-                    <button type="submit" name="submit" class="btn btn-primary"><?php echo $lang['upinterval']?></button>
-                </form>
+                                                                                          include_once 'includes/dbh.inc.php';
+
+                                                                                          $getQuery2 = "SELECT * from autobackup WHERE bID=(SELECT MAX(bID) FROM autobackup);";
+                                                                                          $setQuery2 = mysqli_query($conn, $getQuery2);
+                                                                                          $searchProp = array();
+
+                                                                                          while ($row = mysqli_fetch_assoc($setQuery2)) {
+
+                                                                                            $searchProp[] = array(
+
+                                                                                              $lastbackup = $row['lastbackup'],
+                                                                                              $autointerval = $row['autointerval']
+                                                                                            );
+                                                                                          }
+
+                                                                                          echo $autointerval;
+
+                                                                                          ?></b> days. </p>
+                <div class="line"></div>
+                <div class="col-sm-6 mb-3">
+                  <select name="autointerval" class="form-control" required data-error="Please select the interval.">
+                    <option value=''></option>
+                    <option value='7'><?php echo $lang['every7'] ?></option>
+                    <option value='30'><?php echo $lang['every30'] ?></option>
+                    <option value='60'><?php echo $lang['every60'] ?></option>
+                    <option value='90'><?php echo $lang['every90'] ?></option>
+                  </select>
                 </div>
-              </div>
-              <div class="card">
-                <div class="card-header d-flex align-items-center">
-                  <h4><?php echo $lang['backup']?></h4>
-                </div>
-                <div class="card-body">
-                <form action="includes/backup.inc.php" method="POST" >
-                  <div class="col-sm-6">
-                    <div class="line"></div>
-                    <p><b><?php echo $lang['note']?></b><?php echo $lang['backupnote']?></p>
-                    <div class="line"></div>
-                    <button type="submit" name="submit" class="btn btn-primary"><?php echo $lang['backup']?></button>
-                </form>
-                </div>
-              </div>
-            </div>
+                <button type="submit" name="submit" class="btn btn-primary"><?php echo $lang['upinterval'] ?></button>
+            </form>
+          </div>
         </div>
-                       
-
-    <!-- Manual Modal HTML -->
-    <div id="manualProperty" class="modal fade">
-        <div class="modal-dialog modal-xl">
-            <div class="modal-content">              
-                   <?php
-                   if(isset($_SESSION['lang'])){
-                       if($_SESSION['lang'] == "gr"){
-                           include_once 'manuals/manualPropertiesGreek.html';
-                       }else if ($_SESSION['lang'] == "en"){
-                           include_once 'manuals/manualPropertiesEnglish.html';
-                       }
-                   }                   
-                   ?>
-                    <div class="modal-footer">
-                        <input type="button" class="btn btn-primary" data-dismiss="modal" value="Ok" ?>
-                    </div>
-               
-            </div>
+        <div class="card">
+          <div class="card-header d-flex align-items-center">
+            <h4><?php echo $lang['backup'] ?></h4>
+          </div>
+          <div class="card-body">
+            <form action="includes/backup.inc.php" method="POST">
+              <div class="col-sm-6">
+                <div class="line"></div>
+                <p><b><?php echo $lang['note'] ?></b><?php echo $lang['backupnote'] ?></p>
+                <div class="line"></div>
+                <button type="submit" name="submit" class="btn btn-primary"><?php echo $lang['backup'] ?></button>
+            </form>
+          </div>
         </div>
+      </div>
     </div>
+  </div>
+</div>
+
+
+<!-- Manual Modal HTML -->
+<div id="manualProperty" class="modal fade">
+  <div class="modal-dialog modal-xl">
+    <div class="modal-content">
+      <?php
+      if (isset($_SESSION['lang'])) {
+        if ($_SESSION['lang'] == "gr") {
+          include_once 'manuals/manualPropertiesGreek.html';
+        } else if ($_SESSION['lang'] == "en") {
+          include_once 'manuals/manualPropertiesEnglish.html';
+        }
+      }
+      ?>
+      <div class="modal-footer">
+        <input type="button" class="btn btn-primary" data-dismiss="modal" value="Ok" ?>
+      </div>
+
+    </div>
+  </div>
+</div>
 
 
 
@@ -123,6 +125,9 @@ include_once 'includes/header.inc.php';
 
 
 </div>
+</div>
+
+
 <!-- /.container-fluid -->
 
 </div>
--- restore.php
+++ restore.php
@@ -24,52 +24,54 @@ include_once 'includes/header.inc.php';
     <div class="table-wrapper">
         <div class="table-title">
             <div class="row">
-                </div>
-              </div>
-              <div class="card">
-                <div class="card-header d-flex align-items-center">
-                  <h4><?php echo $lang['restore']?></h4>
-                </div>
-                <div class="card-body">
-                <form action="includes/restore.inc.php" method="POST" enctype="multipart/form-data">
-                  <div class="col-sm-6">
-                  <?php
-                  if(isset($_GET['r'])){
-                  if($_GET['r']=='success'){
-                  echo '<p><b> Successfully Imported. </b></p>';}}
-                  ?>
-                  <div class="line"></div>
-                  <p><b><?php echo $lang['note']?></b><?php echo $lang['noterestore']?></p>
-                    <br><input type="file" name="fileToUpload" id="fileToUpload">
-                    <div class="line"></div><br><br>
-                    <button type="submit" name="submit" class="btn btn-primary"><?php echo $lang['restore']?></button>
-                </form>
-                </div>
-              </div>
             </div>
         </div>
-                       
-
-    <!-- Manual Modal HTML -->
-    <div id="manualProperty" class="modal fade">
-        <div class="modal-dialog modal-xl">
-            <div class="modal-content">              
-                   <?php
-                   if(isset($_SESSION['lang'])){
-                       if($_SESSION['lang'] == "gr"){
-                           include_once 'manuals/manualPropertiesGreek.html';
-                       }else if ($_SESSION['lang'] == "en"){
-                           include_once 'manuals/manualPropertiesEnglish.html';
-                       }
-                   }                   
-                   ?>
-                    <div class="modal-footer">
-                        <input type="button" class="btn btn-primary" data-dismiss="modal" value="Ok" ?>
-                    </div>
-               
+        <div class="card">
+            <div class="card-header d-flex align-items-center">
+                <h4><?php echo $lang['restore'] ?></h4>
+            </div>
+            <div class="card-body">
+                <form action="includes/restore.inc.php" method="POST" enctype="multipart/form-data">
+                    <div class="col-sm-6">
+                        <?php
+                        if (isset($_GET['r'])) {
+                            if ($_GET['r'] == 'success') {
+                                echo '<p><b> Successfully Imported. </b></p>';
+                            }
+                        }
+                        ?>
+                        <div class="line"></div>
+                        <p><b><?php echo $lang['note'] ?></b><?php echo $lang['noterestore'] ?></p>
+                        <br><input type="file" name="fileToUpload" id="fileToUpload">
+                        <div class="line"></div><br><br>
+                        <button type="submit" name="submit" class="btn btn-primary"><?php echo $lang['restore'] ?></button>
+                </form>
             </div>
         </div>
     </div>
+</div>
+
+
+<!-- Manual Modal HTML -->
+<div id="manualProperty" class="modal fade">
+    <div class="modal-dialog modal-xl">
+        <div class="modal-content">
+            <?php
+            if (isset($_SESSION['lang'])) {
+                if ($_SESSION['lang'] == "gr") {
+                    include_once 'manuals/manualPropertiesGreek.html';
+                } else if ($_SESSION['lang'] == "en") {
+                    include_once 'manuals/manualPropertiesEnglish.html';
+                }
+            }
+            ?>
+            <div class="modal-footer">
+                <input type="button" class="btn btn-primary" data-dismiss="modal" value="Ok" ?>
+            </div>
+
+        </div>
+    </div>
+</div>
 
 
 
