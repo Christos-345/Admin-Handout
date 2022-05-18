b223d34b0d0c6311157e1a14ae68a0be49c500b3
Christos Xenofontos <77693529+Christos-345@users.noreply.github.com>
2022-05-19 02:26:04 +0300

    Final Version? Hope so!

--- properties.php
+++ properties.php
@@ -189,6 +189,7 @@ include_once 'includes/header.inc.php';
                                     <label class="col-sm-4 form-control-label text-right"><?php echo $lang['type'] ?> *</label>
                                     <div class="col-sm-6">
                                         <select name="type" class="form-control" required data-error="Please enter this field">>
+                                            <option value="<?php echo $_GET['category'] ?>">Selected: <?php echo $_GET['type'] ?> </option>
                                             <option value="Appliance">Appliance</option>
                                             <option value="Furniture">Furniture</option>
                                             <option value="Clothing">Clothing</option>
@@ -210,6 +211,7 @@ include_once 'includes/header.inc.php';
                                     <div class="col-sm-6 mb-3">
                                         
                                         <select name="town" class="form-control" required data-error="Please enter this field">>
+                                        <option value="<?php echo $_GET['town'] ?>">Selected: <?php echo $_GET['town'] ?> </option>
                                             <option value="Limassol">Limassol</option>
                                             <option value="Larnaca">Larnaca</option>
                                             <option value="Paphos">Paphos</option>
